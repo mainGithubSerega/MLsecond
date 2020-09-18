766d1fde63433de6916b5e6dba7f97b6da0ee167
mainGithubSerega <67060763+mainGithubSerega@users.noreply.github.com>
2020-09-18 22:21:23 +0300

    added

--- js/script.js
+++ js/script.js
@@ -10,13 +10,15 @@ function toggle (columns) {
 toggle('.block-match__item');
 const btn = document.querySelector('.footer__text')
 const popap = document.querySelector('.block-popup')
-
+const body = document.body
 btn.addEventListener('click', function(e){
     e.preventDefault()
     popap.classList.remove('hide')
+    body.classList.add('lock')
 })
 const close = document.querySelector('.block-popup__close')
 close.addEventListener('click', function(){
     popap.classList.add('hide')
+    body.classList.remove('lock')
 });
 
